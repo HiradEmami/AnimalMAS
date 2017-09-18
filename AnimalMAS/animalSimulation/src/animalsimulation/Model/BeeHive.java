d2b09ea5621a10bacb89db532749bfe2829d2104
Hirad Emami Alagha <Hirad.alagha@outlook.com>
2017-09-18 20:12:14 +0200

    Minor old changes

--- AnimalMAS/animalSimulation/src/animalsimulation/Model/BeeHive.java
+++ AnimalMAS/animalSimulation/src/animalsimulation/Model/BeeHive.java
@@ -1,5 +1,7 @@
 package animalsimulation.Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ebombo2
@@ -9,7 +11,9 @@ public class BeeHive {
     public int id;              //unique id for the agent
     public int currentHeight;
     public int currentWidth;
-
+    public ArrayList<BeeScout> scoutBees;
+    
+    
     public void setId(int id) {
         this.id = id;
     }
