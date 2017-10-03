a2718eb193468918f1d0b4bf13ea37f8074727b1
Hirad Emami Alagha <Hirad.alagha@outlook.com>
2017-10-03 16:41:46 +0200

    Worker and Scout Behavior

---
+++ animalSimulation/src/animalsimulation/behavior/WorkerBeeBehavior.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalsimulation.behavior;
+
+/**
+ *
+ * @author Hirad Gorgoroth
+ */
+import animalsimulation.behavior.base.State;
+import animalsimulation.behavior.base.StateMachine;
+import animalsimulation.behavior.event.*;
+import animalsimulation.behavior.movement.BeeMovement;
+import animalsimulation.behavior.movement.BiologicalBeeMovement;
+import animalsimulation.model.base.Agent;
+import animalsimulation.behavior.actions.*;
+
+public class WorkerBeeBehavior  extends StateMachine {
+    
+    public WorkerBeeBehavior(Agent agent) {
+        super(agent);
+        
+        
+        State idle = new State("Idle", null);
+        State gatherFood = new State("GatheringFood", new GatherFood());
+        State dropFood = new State("DroppingFood", new DropFood());
+        State returnToHive = new State("Returning", new BeeMovement());
+        State moveToFoodSource = new State("TravellingToFood", new BiologicalBeeMovement());
+        State communicate = new State("communicate", new Communicate());
+        
+        
+        
+        
+        addStateTransition(idle, communicate, MeetingAgent.class);
+        addStateTransition(communicate, moveToFoodSource, KnowledgeUpdated.class);
+        addStateTransition(moveToFoodSource, gatherFood, DestinationReachedEvent.class);
+        addStateTransition(gatherFood, returnToHive, DestinationReachedEvent.class);
+        addStateTransition(returnToHive, dropFood, DestinationReachedEvent.class);
+        addStateTransition(dropFood, moveToFoodSource, DroppedFood.class);
+        
+        
+        
+
+        setCurrentState(idle); //TODO NEEDS TO CHANGE ACCORDING TO STATE??
+        
+    }
+    
+}
